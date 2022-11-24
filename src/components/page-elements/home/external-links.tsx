d063227d8cdfba3ef3c05564f8c8c39434e6ba94
wholespace214 <wholespace.214@gmail.com>
2022-11-24 23:07:11 +0900

    changed external links

--- src/components/page-elements/home/external-links.tsx
+++ src/components/page-elements/home/external-links.tsx
@@ -78,6 +78,7 @@ const ExternalButtons = styled.div`
   color: ${(props) => props.theme.white};
   text-rendering: optimizeLegibility;
   z-index: 100;
+
   .token-button {
     cursor: pointer;
     color: ${(props) => props.theme.white};
