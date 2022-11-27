4540eb147a584bb527ed06f2bad92ef0d563cfd9
wholespace214 <wholespace.214@gmail.com>
2022-11-27 16:06:32 +0900

    asdfasdf

--- src/components/tokenomics-chart/index.tsx
+++ src/components/tokenomics-chart/index.tsx
@@ -78,6 +78,7 @@ export const MobileTokenChart = () => {
         modifiers: [['brighter', 4]]
       }}
       enableArcLinkLabels={false}
+      tooltip={() => <></>}
       defs={[
         {
           id: 'dots',
